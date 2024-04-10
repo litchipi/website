b23febd231e49b07c59d4d94e38476bdf58db7e2
Litchi Pi <litchi.pi@proton.me>
2024-04-10 19:15:32 +0200

    Add some improvements on the contact form decode tool
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- tools/contact_mail_decode/src/main.rs
+++ tools/contact_mail_decode/src/main.rs
@@ -1,10 +1,27 @@
+use std::collections::HashMap;
+
 #[path = "/home/john/work/web/ecoweb/tools/decode_mailed_data.rs"]
 mod tool;
 
+fn save_to_history(data: HashMap<String, String>) {
+    // TODO    Create a history directory, save the data inside for archives
+}
+
 fn main() {
-    let result = tool::decode_mailed_data("./data".into(), "./privk".into());
+    println!("Copy and paste the mail data over here: ");
+    let mut data = String::new();
+    loop {
+        let n = std::io::stdin().read_line(&mut data).unwrap();
+        if n == 1 {
+            break;
+        }
+    }
+
+    let result = tool::decode_mailed_data(data, "./privk".into());
     println!("Name: {}", result["name"]);
     println!("Email: {}", result["email"]);
     println!("Subject: {}", result["subject"]);
     println!("Body:\n{}\n", result["body"]);
+
+    save_to_history(result);
 }
