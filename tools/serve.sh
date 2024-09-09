b3c2580a06c3ba5c6d95bc85e8d82aa469237458
Litchi Pi <litchi.pi@proton.me>
2024-09-09 15:13:10 +0200

    fixup serve script
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- tools/serve.sh
+++ tools/serve.sh
@@ -9,12 +9,11 @@ ROOT_DIR=$(realpath `dirname $0`/../../../web/ecoweb)
 
 echo "$CONFIG_FILE $ROOT_DIR"
 
-FEATURES_LIST=(dev minify)
-# FEATURES_LIST=(rss humans-txt webring local_storage add-endpoint save-data githook minify)
+FEATURES_LIST=(dev storage-local minify)
 FEATURES=$(IFS=, ; echo "${FEATURES_LIST[*]}")
 
 cd "$ROOT_DIR"
 
 rm -rf ./target/assets
-cargo run "$@" --features "${FEATURES}" -- -c $CONFIG_FILE
+cargo run "$@" --no-default-features --features "${FEATURES}" -- -c $CONFIG_FILE
 # cargo run --release -- -c $CONFIG_FILE
